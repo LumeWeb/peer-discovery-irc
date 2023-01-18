29c4d5f4f84f4331722b315f6ac5f9fdeea8968c
Derrick Hammer <derrick@derrickhammer.com>
2023-01-18 03:06:04 -0500

    *Add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" resolution-mode="require"/>
+import type { Peer } from "@lumeweb/peer-discovery";
+declare const _default: (pubkey: Buffer, options?: {}) => Promise<boolean | Peer>;
+export default _default;
